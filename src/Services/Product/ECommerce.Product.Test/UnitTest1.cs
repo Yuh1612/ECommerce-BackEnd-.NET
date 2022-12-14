db9d3d3b6cb00b91d92d695615f44b968e680b2a
Huy Trần Phan <huytran161201@gmail.com>
2022-12-14 20:59:40 +0700

    add unit test

---
+++ src/Services/Product/ECommerce.Product.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+using ECommerce.Products.API.Services;
+using ECommerce.Products.API.ViewModels.Products.Requests;
+
+namespace ECommerce.Product.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly ProductService productService;
+        private readonly CategoryService categoryService;
+
+        public UnitTest1()
+        {
+        }
+
+        public UnitTest1(ProductService productService, CategoryService categoryService)
+        {
+            this.productService = productService;
+            this.categoryService = categoryService;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsNull(productService);
+            Assert.IsNull(categoryService);
+        }
+    }
+}
